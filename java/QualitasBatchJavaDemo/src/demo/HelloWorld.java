b731f750544fb261f04f837ea77ebd70c7a07031
priya <priya@zawqwe.com>
2023-06-21 18:43:28 +0530

    initial demo commit;

---
+++ java/QualitasBatchJavaDemo/src/demo/HelloWorld.java
@@ -0,0 +1,23 @@
+package demo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello team"); //string
+		System.out.print(12); //number
+		//sysout press ctrl+space
+		
+		System.out.println("hello team" + "goodmrng");
+		//concatenation
+		System.out.println(12); //12
+		
+		System.out.println(2+5); //add 7
+		
+		System.out.println("5"); //string
+		System.out.println("5" +"2");
+		System.out.println(2 +"5");//
+
+	}
+
+}
