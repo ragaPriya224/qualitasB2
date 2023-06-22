f3cae79a8ac5d217c63fad792f0226453e8ac140
priya <priya@zawqwe.com>
2023-06-22 18:59:26 +0530

    datatype demo

---
+++ java/QualitasBatchJavaDemo/src/demo/DataTypeDemo.java
@@ -0,0 +1,35 @@
+package demo;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+//	syntax:	type variableName = value;
+		int choclate = 10;
+//		int -> data type
+//		choclate -> variable name
+//		= -> assigning 
+//		10 -> value
+//		; ->statement ending
+		byte biscuit = 10;
+		// try using short , long 
+		boolean flag = true;
+		char alpha = 'z'; //single quote mandatory
+		float price = 10.08f;
+		System.out.println("hello");
+		System.out.println("choclate");  //choclate
+		System.out.println(choclate);  //10 
+		System.out.println(biscuit); //10
+		System.out.println(flag); //true
+		System.out.println(alpha);  //z
+		int pizza = 20; //initialization
+		int burger;// declare a variable
+		burger = 50;// assign value
+		System.out.println(pizza);
+		burger = 70;
+		System.out.println(burger); //70
+//		int pizza = 50; //duplicate
+		pizza = 50;
+//		System.out.println(PIZZA);// java is casesensitive
+//		int  float = 10;
+	}
+}
